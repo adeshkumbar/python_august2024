43e624ddde2d2ce2d708a90b1555e3a122346e6c
adesh <154432265+adeshkumbar@users.noreply.github.com>
2024-08-21 14:32:19 +0530

    p9.py

--- python_learning/p9.py
+++ python_learning/p9.py
@@ -1,10 +1,10 @@
 #Program to Print Math table of a number
 
-number = input("Enter a number :")
+number = input("Enter a number to print its MATH Table :")
 if  number.isdigit() == True:
     input_number = int(number)
     print("TABLE:\n")
     for integer in range(1,11):
-        print(input_number,"X",integer,"=",input_number*integer)
+        print('%02d * %02d = %03d'%(input_number,integer,input_number*integer))
 else:
         print("Invalid input.Please enter a valid number")
