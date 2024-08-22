2b425b12ad7bb01816c0a48bc910463a693516ec
adesh <154432265+adeshkumbar@users.noreply.github.com>
2024-08-22 09:41:06 +0530

    p22.py

---
+++ python_learning/p22.py
@@ -0,0 +1,15 @@
+
+# Program statement: Program to print X shape of N lines
+
+
+num_lines = int(input("Enter the number of lines:"))
+for i in range(num_lines):
+    for j in range(num_lines):
+        if j == i or j == num_lines-i-1:
+            print("*",end=" ") 
+        else:   
+            print(" ",end=" ")
+    
+    print()
+
+    
